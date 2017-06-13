001d2c84adca5c1755f52cdfc76294043593efc9
qingw <qingw@jumei.com>
2017-06-13 16:20:09 +0800

    1. 修改 AOC 字段的名字。

--- aoc/models.py
+++ aoc/models.py
@@ -5,6 +5,6 @@ from django.db import models
 class ProjectInfo(models.Model):
     id = models.IntegerField(auto_created=True, primary_key=True)
     name = models.CharField(max_length=50)
-    pic = models.IntegerField(default=1000)
+    pic = models.CharField(max_length=1000)
     address = models.CharField(max_length=250)
     des = models.CharField(max_length=1000)
