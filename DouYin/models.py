7cfbe69918c9eb56964cd3f23ebdd6f0d8081305
qingw <qingw@jumei.com>
2017-09-24 15:21:15 +0800

    1. DouYin 添加相关的字段。

--- DouYin/models.py
+++ DouYin/models.py
@@ -2,9 +2,8 @@ from django.db import models
 
 
 # Create your models here.
-class ProjectInfo(models.Model):
+class VideoList(models.Model):
     id = models.IntegerField(primary_key=True).auto_created
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=150)
     pic = models.CharField(max_length=1000)
-    address = models.CharField(max_length=250)
-    des = models.CharField(max_length=1000)
+
