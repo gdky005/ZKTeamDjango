777400942167b334de4d7d3235ba05e017d250dc
qingw <qingw@jumei.com>
2017-09-24 16:32:13 +0800

    1. 修改 bug。

--- DouYin/urls.py
+++ DouYin/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import handler404, handler500
 from DouYin import views
 
 urlpatterns = [
-    url(r'^consume12315_index.html', views.DouYin, name="DouYin"),
+    url(r'^index.html', views.DouYin, name="DouYin"),
     url(r'^video', views.VideoInfo, name="DouYin"),
     url(r'^Json', views.VideoInfoJson, name="DouYin"),
     # handler404 = views.page_not_found,
