28ce346894210ac40d44e0953b5d01e59d8f13d4
qingw <qingw@jumei.com>
2017-09-24 14:41:27 +0800

    1. 添加新的数据库

--- DouYin/models.py
+++ DouYin/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class ProjectInfo(models.Model):
+    id = models.IntegerField(primary_key=True).auto_created
+    name = models.CharField(max_length=50)
+    pic = models.CharField(max_length=1000)
+    address = models.CharField(max_length=250)
+    des = models.CharField(max_length=1000)
