3fcd2d78de570dedef1659d50781c14fee8a70fc
qingw <qingw@jumei.com>
2017-10-18 17:09:33 +0800

    1. 添加日期。

--- JueDiQiuSheng/models.py
+++ JueDiQiuSheng/models.py
@@ -2,11 +2,14 @@ from django.db import models
 
 
 # Create your models here.
+from django.utils import timezone
+
 
 class JDQSCategory(models.Model):
     categoryId = models.IntegerField(primary_key=True, default=1).auto_created
     categoryName = models.TextField(default="")
     categoryUrl = models.TextField(default="")
+    artifactCollection = models.DateTimeField("CollectionTime", default=timezone.now)
 
 
 class JDQSItem(models.Model):
@@ -14,9 +17,10 @@ class JDQSItem(models.Model):
     categoryId = models.ForeignKey(JDQSCategory)
     picUrl = models.TextField(default="")
     artifactName = models.TextField(default="")
-    artifactDate = models.TextField(default="")
+    artifactDate = models.DateTimeField(default=timezone.now)
     artifactSourceUrl = models.TextField(default="")
     artifactUrl = models.TextField(default="")
+    artifactCollection = models.DateTimeField("CollectionTime", default=timezone.now)
 
 
 class JDQSContent(models.Model):
@@ -27,3 +31,4 @@ class JDQSContent(models.Model):
     artifactContent = models.TextField(default="")
     artifactUrl = models.TextField(default="")
     artifactSourceUrl = models.TextField(default="")
+    artifactCollection = models.DateTimeField("CollectionTime", default=timezone.now)
