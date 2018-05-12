109a80ced63fd2689a9dca5d56d3a02a5abb3dd6
qingw <qingw@jumei.com>
2018-05-12 22:58:38 +0800

    1. 添加商品展示页面。

--- Shop/views.py
+++ Shop/views.py
@@ -6,4 +6,4 @@ from Shop import models
 
 def shop(request):
     projects = models.ShopInfo.objects.all()
-    return render(request, 'index_shop.html')
+    return render(request, 'index_shop.html', {"shops": projects})
