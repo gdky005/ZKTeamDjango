9df4855a3e4a6f699d5003123493f2026bfb820a
qingw <qingw@jumei.com>
2018-05-14 17:20:52 +0800

    1. 修改和添加字段； 2.

--- Shop/models.py
+++ Shop/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 
 # Create your models here.
+from django.utils import timezone
+
+
 class ShopInfo(models.Model):
-    id = models.IntegerField(auto_created=True, primary_key=True)
-    pid = models.CharField(max_length=50, default='')
-    name = models.CharField(max_length=50)
-    price = models.IntegerField(default=0)
-    des = models.CharField(max_length=50)
+    id = models.AutoField(auto_created=True, primary_key=True)
+    pid = models.TextField(max_length=50)
+    name = models.TextField(max_length=50, default='', null=True)
+    price = models.IntegerField(default=0, null=True)
+    des = models.TextField(max_length=50, default='', null=True)
+    add_date = models.DateTimeField(u'添加时间', auto_now_add=True, editable=True, null=True)
+    update_time = models.DateTimeField(u'更新时间', auto_now=True, null=True)
