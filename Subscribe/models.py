5cacf560f8cb8193247fb254726ff7609b972deb
qingw <qingw@jumei.com>
2018-07-31 19:04:52 +0800

    1. 修复数据问题。

--- Subscribe/models.py
+++ Subscribe/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class SubInfo(models.Model):
     id = models.AutoField(auto_created=True, primary_key=True)
@@ -9,3 +10,29 @@ class SubInfo(models.Model):
     des = models.TextField(max_length=50, default='', null=True)
     add_date = models.DateTimeField(u'添加时间', auto_now_add=True, editable=True, null=True)
     update_time = models.DateTimeField(u'更新时间', auto_now=True, null=True)
+
+
+class SubMovieInfo(models.Model):
+    # id = models.AutoField(auto_created=True, primary_key=True)
+    pid = models.IntegerField(primary_key=True, default=1).auto_created
+    name = models.TextField(max_length=50, default='', null=True)
+    pic = models.TextField(max_length=50, default='', null=True)
+    url = models.TextField(max_length=50, default='', null=True)
+    update_time = models.TextField(max_length=50, default='', null=True)
+    intro = models.TextField(max_length=50, default='', null=True)
+    capture_pic = models.TextField(max_length=50, default='', null=True)
+
+
+class SubMovieDownload(models.Model):
+    id = models.IntegerField(primary_key=True, default=1).auto_created
+    pid = models.ForeignKey(SubMovieInfo)
+    fj_name = models.TextField(max_length=50, default='', null=True)
+    fj_download_url = models.TextField(max_length=50, default='', null=True)
+
+
+class SubMovieLastestInfo(models.Model):
+    id = models.IntegerField(primary_key=True, default=1).auto_created
+    pid = models.ForeignKey(SubMovieInfo)
+    fj_number = models.TextField(max_length=50, default='', null=True)
+    # fj_name = models.ForeignKey(SubMovieDownload)
+    # fj_download_url = models.ForeignKey(SubMovieDownload)
