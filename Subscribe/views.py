88fe98637a18f3127bbaf18fa03fa157a2427934
qingw <qingw@jumei.com>
2018-08-04 09:52:50 +0800

    1. 添加调试信息。

--- Subscribe/views.py
+++ Subscribe/views.py
@@ -15,10 +15,59 @@ from dss.Serializer import serializer
 from django.contrib import auth
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponseRedirect
+from utils.Email import send
+
+
+
+
+import hashlib
+import json
+from lxml import etree
+from django.utils.encoding import smart_str
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
 
 
 # Create your views here.
-from utils.Email import send
+
+
+WEIXIN_TOKEN = 'write-a-value'
+
+
+# def weixin_main(request):
+#     """
+#     所有的消息都会先进入这个函数进行处理，函数包含两个功能，
+#     微信接入验证是GET方法，
+#     微信正常的收发消息是用POST方法。
+#     """
+#     if request.method == "GET":
+#         signature = request.GET.get("signature", None)
+#         timestamp = request.GET.get("timestamp", None)
+#         nonce = request.GET.get("nonce", None)
+#         echostr = request.GET.get("echostr", None)
+#         token = WEIXIN_TOKEN
+#         tmp_list = [token, timestamp, nonce]
+#         tmp_list.sort()
+#         tmp_str = "%s%s%s" % tuple(tmp_list)
+#         tmp_str = hashlib.sha1(tmp_str).hexdigest()
+#         if tmp_str == signature:
+#             return HttpResponse(echostr)
+#         else:
+#             return HttpResponse("weixin  index")
+#     else:
+#         xml_str = smart_str(request.body)
+#         request_xml = etree.fromstring(xml_str)
+#         response_xml = auto_reply_main(request_xml)# 修改这里, 自己完成自己的消息。
+#         return HttpResponse(response_xml)
+
+
+
+
+
+
+
+
+
 
 
 @login_required
