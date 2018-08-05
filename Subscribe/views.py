9e856d7fc9c4e2739cbe49746ce45e10e9cbc229
qingw <qingw@jumei.com>
2018-08-05 14:14:02 +0800

    1. 调优数据格式。

--- Subscribe/views.py
+++ Subscribe/views.py
@@ -64,19 +64,15 @@ from utils.Email import send
 import hashlib
 
 
-
 # 测试数据：/Subscribe/weiXin/?signature=5e1c55f68fa10321419b62b171d3518a398096f3&echostr=7370345721803176943&timestamp=1533393832&nonce=2102056081, zkteam 服务器返回类似：6801932741839289079
 # 微信服务器的信息是：signature=5e1c55f68fa10321419b62b171d3518a398096f3, timestamp=1533393832, nonce=2102056081, echostr=7370345721803176943
-
 def wxTemplateMsg(request):
-    openid = 'oQrNzwaFHdvdufYEGZhz4cNwhznk'
+    openid = 'oQrNzwaFHdvdufYEGZhz4cNwhznk' # 孤独狂饮的openid
     # template_id = '5U1Ykxvb00WBt9WpMpaBKFpC3UFszbhQHGaZ9alczy0' #订阅模板消息
     template_id = '4KOE8MczMCka1CW_q_BcegEzBVrAacFv81oEeVNTPRw' #预约服务提醒
     url = "http://www.zkteam.cc"
-    # url = "http://www.zkteam.cc"
-    # url = ""
-    # data = '{"first":"你好呀，这是订阅号消息first","keyword1":"你好呀，这是订阅号消息keyword1","keyword2":"你好呀，这是订阅号消息keyword2","remark":"你好呀，remark"}'
     data = {
+        "content": {"value": "content！", "color": "#173177"},
         "first": {"value": "恭喜你购买成功！", "color": "#173177"},
         "keyword1": {"value": "这是预约内容！", "color": "#173177"},
         "keyword2": {"value": "服务说明！", "color": "#173177"},
@@ -84,21 +80,15 @@ def wxTemplateMsg(request):
     }
 
     templeUrl = 'https://api.weixin.qq.com/cgi-bin/message/template/send'
-    getTemplateId = 'https://api.weixin.qq.com/cgi-bin/template/get_all_private_template'
     token = WXConstant.wx_access_token
 
-    paramsData = {}
+    paramsData = {"access_token": token,
+                  "touser": openid,
+                  "template_id": template_id,
+                  "url": url,
+                  "data": data}
 
-    paramsData["access_token"] = token
-    paramsData["touser"] = openid
-    paramsData["template_id"] = template_id
-    paramsData["url"] = url
-    paramsData["data"] = data
-
-    params = json.dumps(paramsData)
-    params = json.loads(params)
-
-    result = requests.post(templeUrl + "?access_token=" + token, json=params).json()
+    result = requests.post(templeUrl + "?access_token=" + token, json=paramsData).json()
     return getHttpResponse(0, "ok", result)
 
 
