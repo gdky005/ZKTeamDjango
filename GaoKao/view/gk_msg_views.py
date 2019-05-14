bf88fa496da521877dfe21b72856394281946537
WangQing <wangqing11@100tal.com>
2019-05-14 18:53:37 +0800

    1. 添加消息列表的分页数据。

--- GaoKao/view/gk_msg_views.py
+++ GaoKao/view/gk_msg_views.py
@@ -2,22 +2,11 @@ from django.views.decorators.csrf import csrf_exempt
 from pymysql import Error
 
 from GaoKao.model import GKMsgInfo
-from GaoKao.view.base_views import getHttpResponse
+from GaoKao.view.base_views import getHttpResponse, getPagingData
 
 
 def JsonGKListView(request):
-    maxData = 5
-    count = request.GET.get("pageCount")
-    if count:
-        maxData = int(count)
-
-    try:
-        project_info = GKMsgInfo.objects.values()[:maxData]  # 取出该表所有的数据
-        projects = list(project_info)
-
-        return getHttpResponse(0, "ok", projects)
-    except Error:
-        return getHttpResponse(10000, "Error", "")
+    return getPagingData(request, GKMsgInfo)
 
 
 @csrf_exempt
