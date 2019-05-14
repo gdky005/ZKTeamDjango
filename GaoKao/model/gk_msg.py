c920b35b17350ee082d301d6b08a80864e460611
WangQing <wangqing11@100tal.com>
2019-05-14 10:42:19 +0800

    1. 添加消息表信息。

--- GaoKao/model/gk_msg.py
+++ GaoKao/model/gk_msg.py
@@ -1,3 +1,5 @@
+from django.utils import timezone
+
 from django.db import models
 
 
@@ -5,5 +7,10 @@ class GKMsgInfo(models.Model):
     id = models.IntegerField(primary_key=True).auto_created
     title = models.TextField()
     imgUrl = models.TextField()
-    url = models.TextField()
+    url = models.TextField(default="")
+    uid = models.IntegerField(default=0)
+    author = models.TextField(default="")
+    summary = models.TextField(default="")
+    des = models.TextField(default="")
+    createTime = models.DateTimeField("createTime", default=timezone.now)
 
