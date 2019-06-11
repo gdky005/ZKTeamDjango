fdbd59bf525462430696043425ddb0c0e5d5f1a7
WangQing <wangqing11@100tal.com>
2019-06-11 11:08:38 +0800

    1. 添加热门数据的数据库。

--- ManHua/models.py
+++ ManHua/models.py
@@ -14,3 +14,11 @@ class Category(models.Model):
     mid = models.IntegerField()
     name = models.TextField()
     url = models.TextField()
+
+
+class HotData(models.Model):
+    id = models.IntegerField(primary_key=True).auto_created
+    mid = models.IntegerField()
+    name = models.TextField()
+    picUrl = models.TextField()
+    newPage = models.TextField()
