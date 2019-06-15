43a882ca49373677e8a7c34c5bebde317b2e501d
WangQing <wangqing11@100tal.com>
2019-06-15 23:30:30 +0800

    1. 添加 漫画接口的缓存，接口默认缓存 5 分钟。

--- ManHua/views.py
+++ ManHua/views.py
@@ -1,31 +1,38 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
 
-from GaoKao.model import GKNewsInfo
 from ManHua import models
 from ManHua.base_views import getPagingData
 from ManHua.models import Category, HotData, MHDetail, MHDetailChapter, MHChapterPic
 
 
+@cache_page(60 * 15)  # 秒数，这里指缓存 15 分钟，不直接写900是为了提高可读性
 def ManHuaIndex(request):
     projects = models.ManHua.objects.all()
     return render(request, 'manhua_index.html', {"projects": projects})
 
 
+@cache_page(60 * 5)
 def JsonMHCategoryView(request):
+    print("开始读取 分类 数据")
     return getPagingData(request, Category)
 
 
+@cache_page(60 * 5)
 def JsonMHHotDataView(request):
     return getPagingData(request, HotData)
 
 
+@cache_page(60 * 5)
 def JsonMHDetailView(request):
     return getPagingData(request, MHDetail)
 
 
+@cache_page(60 * 5)
 def JsonMHDetailChapterView(request):
     return getPagingData(request, MHDetailChapter)
 
 
+@cache_page(60 * 5)
 def JsonMHChapterPicView(request):
     return getPagingData(request, MHChapterPic)
--- ZKTeam/settings.py
+++ ZKTeam/settings.py
@@ -64,6 +64,16 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379/1",
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         }
+#     }
+# }
+
 ROOT_URLCONF = 'ZKTeam.urls'
 
 TEMPLATES = [
