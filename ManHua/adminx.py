b48495f42ca3d7cc3826f5f6c815ec87c4ff67b9
WangQing <wangqing11@100tal.com>
2019-06-16 18:22:44 +0800

    1. 注册添加 CategoryForCategoryId 到 xadmin

--- ManHua/adminx.py
+++ ManHua/adminx.py
@@ -1,11 +1,12 @@
 import xadmin
 
 # Register your models here.
-from ManHua.models import Category, MHDetail, MHDetailChapter, MHChapterPic, MHBanner
+from ManHua.models import Category, MHDetail, MHDetailChapter, MHChapterPic, MHBanner, CategoryForCategoryId
 
 xadmin.site.register(Category)
 xadmin.site.register(MHDetail)
 xadmin.site.register(MHDetailChapter)
 xadmin.site.register(MHChapterPic, MHChapterPic.ShowXItem)
 xadmin.site.register(MHBanner)
+xadmin.site.register(CategoryForCategoryId)
 
