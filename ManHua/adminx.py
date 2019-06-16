c8c6a6c390b56430f224df340207e09a1fa01560
WangQing <wangqing11@100tal.com>
2019-06-16 16:43:33 +0800

    1. 注册 漫画的推荐到 后台，可以动态配置。

--- ManHua/adminx.py
+++ ManHua/adminx.py
@@ -1,10 +1,11 @@
 import xadmin
 
 # Register your models here.
-from ManHua.models import Category, MHDetail, MHDetailChapter, MHChapterPic
+from ManHua.models import Category, MHDetail, MHDetailChapter, MHChapterPic, MHBanner
 
 xadmin.site.register(Category)
 xadmin.site.register(MHDetail)
 xadmin.site.register(MHDetailChapter)
 xadmin.site.register(MHChapterPic, MHChapterPic.ShowXItem)
+xadmin.site.register(MHBanner)
 
