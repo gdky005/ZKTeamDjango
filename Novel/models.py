94f8c44e0877ae0cd61ad3f3dab828b6f5ef1b85
WangQing <wangqing11@100tal.com>
2019-09-02 17:46:20 +0800

    1. 添加详情页面的数据结构。

--- Novel/models.py
+++ Novel/models.py
@@ -8,3 +8,13 @@ class NovelData(models.Model):
     name = models.TextField()
     url = models.TextField()
     sourceUrl = models.TextField(default="")
+
+
+class NovelDetailData(models.Model):
+    id = models.IntegerField(primary_key=True).auto_created
+    pid = models.IntegerField()
+    name = models.TextField()
+    author = models.TextField()
+    content = models.TextField()
+    url = models.TextField()
+    sourceUrl = models.TextField()
